4201930f29b84d25d8b2f2a2a6152215e05ab263
Richard Harris <rich.harris@bristol.ac.uk>
2017-02-10 18:41:27 +0000

    Tidied up print output for the model summary

--- R/summary.R
+++ R/summary.R
@@ -4,24 +4,16 @@ print.index <- function(x, ...) {
 }
 
 summary.index <- function(x, ...) {
-  cat("\nThe index of dissimilarity is",format(round(x[1], 3), nsmall = 3))
-  cat("\nfor variables",attr(x, "vars"),"\n")
-  if (length(x) == 2) cat("with an expected value under randomisation of",x[2])
+  cat(paste(attr(x, "vars"), collapse=" ~ "),"\n")
+  zz <- data.frame(ID = round(x[1], 3), E_ID = round(x[2], 3))
+  rownames(zz) <- ""
+  print(format(zz, nsmall=3))
+  cat("\n")
   if (!is.null(attr(x, "variance"))) {
     v <- attr(x, "variance")
-    txt <- "\nThe percentage of the variance at each level is:\n"
-    for(i in 1:length(v)) {
-      txt <- paste(txt, names(v[i]),"\t\t",format(round(v[i], 2), nsmall = 2),"\n",sep="")
-    }
-    cat(txt)
-  }
-  if (!is.null(attr(x, "holdback"))) {
-    v <- attr(x, "holdback")
-    txt <- "\nPercentage change in the ID if the contribution of the level is set to zero:\n"
-    for(i in 1:length(v)) {
-      txt <- paste(txt, names(v[i]),"   ",format(round(v[i], 2), nsmall = 2),"\n",sep="")
-    }
-    cat(txt)
+    h <- attr(x, "holdback")
+    zz <- data.frame("Pvariance" = v, "Holdback" = h)
+    print(format(zz))
   }
 }
 
