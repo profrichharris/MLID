1117ae656ba156c6c8a71929b38ebac9a6f521d3
Richard Harris <rich.harris@bristol.ac.uk>
2017-02-13 06:14:35 +0000

    Add R-sq for predicted changes

--- R/predict.R
+++ R/predict.R
@@ -5,7 +5,7 @@ predict.index <- function(index, exclude) {
   mlm <- attr(index, "mlm")
   data <- slot(mlm, "frame")
   rr <- rvals(mlm)
-  id <- 0.5*sum(abs(rowSums(rr)))
+  id <- attr(index, "id")
 
   drop <- lapply(exclude, function(x, df = data, rr = rr) {
     k <- which(apply(df, 2, function(y) any(y == x)))
@@ -13,13 +13,19 @@ predict.index <- function(index, exclude) {
     return(list(j, k))
   })
 
+  dummies <- matrix(0, ncol = ncol(rr), nrow = nrow(rr))
+
   for(i in 1:length(drop)) {
 
     j <- drop[[i]][[1]]
     k <- drop[[i]][[2]]
     rr[j, k] <- 0
+    dummies[j, k] <- 1
 
   }
+
+  ols2 <- lm(data$y ~ 0 + dummies, offset = data$"(offset)")
+  r2 <- summary(ols2)$r.squared
 
   newid <- 0.5*sum(abs(rowSums(rr)))
   diff <- newid - id
@@ -27,5 +33,6 @@ predict.index <- function(index, exclude) {
   df <- data.frame(ID = c(id, newid, diff, pcntchg))
   rownames(df) <- c("before", "after", "difference", "% change")
   print(format(round(df,3), nsmall = 3))
+  cat("\nR-squared of",round(r2,3))
 
 }
