c22ea0f0194510383efc3f796bf7fb4f0800bd95
Richard Harris <rich.harris@bristol.ac.uk>
2017-02-14 14:12:25 +0000

    Moved to different functions

---
+++ R/printindex.R
@@ -0,0 +1,21 @@
+
+
+print.index <- function(x, ...) {
+  cat(paste(attr(x, "vars")[1:2], collapse=" ~ "),"\n")
+  zz <- data.frame(ID = round(x[1], 3), E_ID = round(x[2], 3))
+  rownames(zz) <- ""
+  print(format(zz, nsmall=3))
+  cat("\n")
+  if (!is.null(attr(x, "variance"))) {
+    v <- attr(x, "variance")
+    h <- attr(x, "holdback")
+    zz <- data.frame("Pvariance" = v, "Holdback" = h)
+    print(format(zz))
+  }
+}
+
+
+
+
+
+
